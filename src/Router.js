dd70cb5c661f1c057679f143fbb4683579454354
kjschwoerke <kjschwoerke@yahoo.com>
2020-02-23 20:03:12 -0600

    Added Log-In feature with cookies

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { Switch, Route } from 'react-router'
+import { Switch, Route, Redirect } from 'react-router'
 import Home from './containers/Home'
-import Car from './containers/Business'
+import Business from './containers/Business'
 import Dashboard from './containers/Dashboard'
+import Login from './components/Login.js'
+import cookie from 'cookie'
+
+
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
+
+
+const ProtectedRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route
+        {...rest}
+        render={(props) => checkAuth() === true
+            ? <Component {...props} />
+            : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+            />
+    )
+}
+
+
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/business/:id" component={Car} />
-            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/business/:id" component={Business} />
+            <Route path="/login" component={Login} />
+            <ProtectedRoute path="/dashboard" component={Dashboard} />
         </Switch>
     );
 };
